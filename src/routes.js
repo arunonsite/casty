d98b3459e88a2a470fd4986ea52a32ae7fa27b93
arun <arunonsite@gmail.com>
2020-02-25 21:44:26 +0530

    Roles Controll over Menu

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Redirect } from "react-router-dom";
 import { Route } from 'react-router-dom';
 
 import { isUserAuthenticated, getLoggedInUser } from './helpers/authUtils';
-
+import NotFound from './components/NotFound';
 // lazy load all the views
 const Dashboard = React.lazy(() => import('./pages/dashboards/'));
 const Users = React.lazy(() => import('./pages/users/'));
@@ -29,18 +29,18 @@ const PrivateRoute = ({ component: Component, roles, ...rest }) => (
       // not logged in so redirect to login page with the return url
       return <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
     }
-
     const loggedInUser = getLoggedInUser();
     // check if route is restricted by role
     if (roles && roles.indexOf(loggedInUser.role) === -1) {
       // role not authorised so redirect to home page
-      return <Redirect to={{ pathname: '/' }} />
+      return <NotFound  {...props} />
     }
-
     // authorised so return component
     return <Component {...props} />
   }} />
 )
+
+
 
 const routes = [
   // auth and account
@@ -51,19 +51,36 @@ const routes = [
   { path: '/confirm', name: 'Confirm', component: ConfirmAccount, route: Route },
 
   // other pages
-  { path: '/dashboard', name: 'Dashboard', component: Dashboard, route: PrivateRoute, roles: ['Admin'] },
-  { path: '/users', name: 'Users', component: Users, route: PrivateRoute, roles: ['Admin'] },
-  { path: '/company', name: 'Users', component: Company, route: PrivateRoute, roles: ['Admin'] },
-  { path: '/channels', name: 'Channels', component: Channels, route: PrivateRoute, roles: ['Admin'] },
-  { path: '/episodes', name: 'Episodes', component: Episodes, route: PrivateRoute, roles: ['Admin'] },
-  { path: '/shows', name: 'Shows', component: Shows, route: PrivateRoute, roles: ['Admin'] },
-  {
+  { path: '/dashboard', icon:'fe-airplay',  name: 'Dashboard', component: Dashboard, route: PrivateRoute, roles: [ 'SuperAdmin'] },
+  { path: '/users',icon:'fe-users',  name: 'Users', component: Users, route: PrivateRoute, roles: ['Admin'] },
+  { path: '/channels',icon:'fe-play-circle',  name: 'Channels', component: Channels, route: PrivateRoute, roles: ['Admin'] },
+  { path: '/episodes',icon:'fe-grid',  name: 'Episodes', component: Episodes, route: PrivateRoute, roles: ['Admin'] },
+  { path: '/shows',icon:'fe-film',  name: 'Shows', component: Shows, route: PrivateRoute, roles: ['Admin'] },
+  
+  
+]
+const loggedInUser = getLoggedInUser();
+ console.log("loggedInUser--->>", loggedInUser);
+  const {role=''} = loggedInUser;
+  const dahs= {
     path: "/",
     exact: true,
     component: () => <Redirect to="/dashboard" />,
     route: PrivateRoute
-  },
-  
-]
+  };
+  const usesrs= {
+    path: "/",
+    exact: true,
+    component: () => <Redirect to="/users" />,
+    route: PrivateRoute
+  };
+  const dashROles = ['super'];
+  if (dashROles && dashROles.indexOf(role) === -1) {
+    // role not authorised so redirect to home page
+    routes.push(usesrs);
+  } else{
+    routes.push(usesrs);
+  }
+ 
 
 export { routes, PrivateRoute };
